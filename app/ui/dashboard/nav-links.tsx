c4d788794569d4d8d800152793315f0f3218d73f
TomGrout <c3034731@hallam.shu.ac.uk>
2024-05-16 15:39:40 +0100

    4

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  CalculatorIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -19,7 +20,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-  { name: 'Bustoners', href: '/dashboard/invoices', icon: UserGroupIcon },
+  { name: 'Computer', href: '/dashboard/cleverboy', icon: CalculatorIcon },
 
 ];
 
